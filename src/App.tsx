0137490d7825c6ea7f5dee8aa858f083605ab771
Min Cai <min.cai.china@gmail.com>
2020-09-01 23:09:50 +0800

    Add navbar, breadcrumb and footer in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,91 @@
 import React from 'react';
+import {SimpleFooter} from "@itecgo/blocks/dist/components/SimpleFooter/SimpleFooter";
+import {SimpleBreadcrumb} from "@itecgo/blocks/dist/components/SimpleBreadcrumb/SimpleBreadcrumb";
+import {SimpleNavbar} from "@itecgo/blocks/dist/components/SimpleNavbar/SimpleNavbar";
 
 export class App extends React.Component {
   render() {
     return (
-        <div className="App">
-          <header className="App-header">
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+        <div>
+            <SimpleNavbar
+                brand={{
+                    title: "蚁克造",
+                    href: "/"
+                }}
+                items={[
+                    {
+                        title: "员工",
+                        href: "/",
+                        active: true
+                    },
+                    {
+                        title: "客户",
+                        href: "/",
+                        active: true
+                    },
+                    {
+                        title: "文档",
+                        href: "/",
+                        active: true
+                    },
+                    {
+                        title: "图纸",
+                        href: "/",
+                        active: true
+                    },
+                    {
+                        title: "报价单",
+                        href: "/",
+                        active: true
+                    },
+                    {
+                        title: "订单",
+                        href: "/",
+                        active: true
+                    },
+                    {
+                        title: "加工",
+                        href: "/",
+                        active: true
+                    },
+                    {
+                        title: "设置",
+                        href: "/",
+                        active: true
+                    },
+                    {
+                        title: "日志",
+                        href: "/",
+                        active: true
+                    },
+                    {
+                        title: "统计",
+                        href: "/",
+                        active: true
+                    },
+                    {
+                        title: "帮助",
+                        href: "/",
+                        active: true
+                    },
+                ]}
+            />
+
+            <SimpleBreadcrumb items={[
+                {
+                    title: "主页",
+                    href: "/"
+                },
+                {
+                    title: "员工管理",
+                    active: true
+                },
+            ]}/>
+
+            <SimpleFooter brand={{
+                title: "蚁克造 (C) 2020",
+                href: "http://www.ikzao.com:3000/"
+            }}/>
         </div>
     );
   }
