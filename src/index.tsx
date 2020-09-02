3455409c2996aa5031b83ad7a56281792d531a3d
Min Cai <min.cai.china@gmail.com>
2020-09-02 16:42:43 +0800

    Update IkzaoApp.

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import {IkzaoApp} from './IkzaoApp';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {ProtocolConverterApp} from "./ProtocolConverterApp";
 
 ReactDOM.render(
   <React.StrictMode>
     <IkzaoApp />
-    <ProtocolConverterApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
