2ba367972c96516a11ef61bcb1aeee8c5941e8ae
Min Cai <min.cai.china@gmail.com>
2020-09-02 17:56:41 +0800

    Add SimpleToast.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {IkzaoApp} from './IkzaoApp';
+import {App} from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <IkzaoApp />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
